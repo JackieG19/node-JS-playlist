91a64ad08b01ae3349053c4538f8303774c08a43
Jackie <JGtheophila19@gmail.com>
2019-01-03 14:40:24 -0500

    Modules and require()

--- node.js
+++ node.js
@@ -4,21 +4,3 @@ var counter = function(arr){
 	
 module.exports = counter;
 	
-
-var stuff = require('./stuff');
-	
-console.log(stuff.counter()['shaun','crystal','ryu']);
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi, 5)); 
-	
-
-
-var adder = function(a,b){
-    return 'the sum of the 2 numbers is ${a+b}';
-};
-	
-var pi = 3.142;
-	
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
