4cf159a45101b4cf1c41cad59ffab1df81ad46f5
Jackie <jacgut19@gmail.com>
2019-01-03 20:07:22 +0000

    object literal notation with an export

--- node.js
+++ node.js
@@ -1,11 +1,19 @@
-module.exports.counter = function(arr){
-    return 'There are ' + arr.length + 'elements in this array';
+var counter = function(arr){
+    return 'There are ' + arr.length + ' elements in this array';
 };
 
-module.exports.adder = function(a,b){
-    return 'the sum of the 2 numbers is ${a+b}';
+
+var adder = function(a,b){
+  return 'The sum of the 2 number is ${a+b}';  
 };
 
-module.exports.pi = 3.142;
 
-// another way to export is to reference theses modules to the functions
+var pi = 3.142;
+
+// object literal notation with an export
+module.exports = {
+  counter: counter,
+  adder: adder,
+  pi: pi 
+};
+
