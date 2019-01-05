8e4f0dbaaec482c8330f781794cfcded4373e5b7
Jackie <jacgut19@gmail.com>
2019-01-05 05:03:22 +0000

    creating a folder, read a file and create a file in the folder

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 
 var fs = require('fs');
 
-// asynchronous versions in Creating & Removing Directories
+// asynchronous versions 
 
-fs.mkdirSync('stuff');
-// creates a folder(directory) called stuff
+fs.mkdir('stuff', function(){
+// creates a directory called stuff
+   fs.readFile('readMe.txt', 'utf8', function(err, data){
+    // read the readMe file
+       fs.writeFile('./stuff/writeMe.txt', data);
+       // makes a writeMe file inside the stuff directory
+   }) ;
+});
 
-fs.rmdirSync('stuff');
+
+
+
+
+
+
+// fs.mkdirSync('stuff');
+//creates a folder(directory) called stuff
+
+// fs.rmdirSync('stuff');
 // removes the folder(directory) called stuff
-
-
 
 // fs.unlink('writeMe.txt'); 
 // in terminal: node app = writeMe.txt is deleted
