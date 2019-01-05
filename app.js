cc13fcd9b6d183ad72ca6124feea723b07923368
Jackie <jacgut19@gmail.com>
2019-01-05 04:46:44 +0000

    Creating & Removing Directories

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+
 var fs = require('fs');
 
-fs.unlink('writeMe.txt'); 
+// asynchronous versions in Creating & Removing Directories
+
+fs.mkdirSync('stuff');
+// creates a folder(directory) called stuff
+
+fs.rmdirSync('stuff');
+// removes the folder(directory) called stuff
+
+
+
+// fs.unlink('writeMe.txt'); 
 // in terminal: node app = writeMe.txt is deleted
 
